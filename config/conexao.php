c7464b6925081fdce6003219a47efe68560d74f2
Kauã <kauanasc2008@gmail.com>
2026-04-09 15:03:46 -0300

    Finalizando /conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -1,12 +1,16 @@
 <?php
-$host = "10.91.47.129";
-$db = "servicehubdb01";
-$user = "root";
-$pass = "P@ssw0rd";
+$host = "10.91.47.129"; //IP Server
+$db = "servicehubdb01"; //Nome DB
+$user = "root";         //Usuário
+$pass = "202720";     //Senha
 
 try{
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8",$user,$pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    // echo "Sucess";
+    // var_dump($pdo);
 }catch(PDOException $e){
+    // var_dump($e->getMessage());
     die("Erro na conexão: ".$e->getMessage());
 }
 
