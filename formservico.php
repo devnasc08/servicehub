1c8aea995f70ab7563ce5c2b69590973c53fdd7c
Kauã <kauanasc2008@gmail.com>
2026-04-10 16:48:21 -0300

    Adicionando Formulário de serviços

---
+++ formservico.php
@@ -0,0 +1,59 @@
+<?php 
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+require_once "config/conexao.php";
+$nome = $_POST ['txtnome'];
+$descricao = $_POST ['txtdescricao'];
+$preco = $_POST ['txtpreco'];
+
+// ==== Inserindo Serviços ==== 
+$sql = "insert servicos (nome, descricao, preco) values(:nome, :descricao, :preco)";
+$cmd = $pdo->prepare($sql);
+$cmd->execute([':nome'=>$nome, ':descricao'=>$descricao, ':preco'=>$preco]);
+$id = $pdo->lastInsertId();     //Armazena o ID do serviço que acabou de ser inserido
+
+if(isset($id))/*SE valor associado em $ID?*/{
+    echo "Serviço cadastrado com sucesso, com id ".$id;
+}
+
+else{
+    echo "Falha ao cadastrar o serviço!!";
+ }
+}
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário de Serviços</title>
+</head>
+<body>
+    <form action="formservico.php" method="post">
+        <input type="number" name="txtid" id="" hidden>
+ 
+        <label for="txtnome">Nome</label>
+        <input type="text" name="txtnome" id="">
+ 
+        <label for="txtdescricao">Descrição</label>
+        <input type="text" name="txtdescricao">
+ 
+        <label for="txtpreco">Preço</label>
+        <input type="text" name="txtpreco">
+ 
+        <button type="submit">Gravar</button>
+
+
+    </form>
+</body>
+</html>
+
+ 
+ 
+ 
+ 
+ 
