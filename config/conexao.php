b738549ec73f5daa2f428c339edb3c6e00c8dd23
Kauã <kauanasc2008@gmail.com>
2026-04-14 14:33:37 -0300

    adicionanado função obterPdo | aprendendo classe

--- config/conexao.php
+++ config/conexao.php
@@ -1,8 +1,11 @@
 <?php
+function obterPdo():PDO{
+
 $host = "10.91.47.129"; //IP Server
 $db = "servicehubdb01"; //Nome DB
 $user = "root";         //Usuário
 $pass = "202720";     //Senha
+static $pdo;
 
 try{
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8",$user,$pass);
@@ -13,5 +16,11 @@ try{
     // var_dump($e->getMessage());
     die("Erro na conexão: ".$e->getMessage());
 }
+
+return $pdo;
+
+
+}
+
 
 ?>
