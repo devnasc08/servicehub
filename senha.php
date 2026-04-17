64e8932a12604ea950bd85a41d3a0232b68f2085
Kauã <kauanasc2008@gmail.com>
2026-04-17 15:42:52 -0300

    Executando Listar e Buscar

--- senha.php
+++ senha.php
@@ -5,16 +5,34 @@
 
 require_once "class/usuario.php";
 
+// $usuario ->setNome('Milharino Santos');
+// $usuario ->setEmail('mil@harino.sa');
+// $usuario ->setSenha('mi2026@TV');
+// $usuario ->setTipo(2);
+
+// // if($usuario->inserir()){
+//     //     echo "Usuario".$usuario->getNome()."Inserido com o ID".$usuario->getId();
+//     // }
+//     // echo "<prev>";
+//     // print_r($usuario->listar());
+    
+
+
+     echo "<prev>";
+     foreach(Usuario::listar() as $user){
+     echo $user ['id']."-".$user['nome']."<br>";
+     }
+
+
+
 $usuario = new Usuario();
-$usuario ->setNome('Milharino Santos');
-$usuario ->setEmail('mil@harino.sa');
-$usuario ->setSenha('mi2026@TV');
-$usuario ->setTipo(2);
-
-if($usuario->inserir()){
-    echo "Usuario".$usuario->getNome()."Inserido com o ID".$usuario->getId();
+if($usuario->buscarPorId(62)){
+    echo"<prev>";
+    echo $usuario->getId()."-".$usuario->getNome()."<br>";
 }
-
+else{
+    echo "Usuário não encontrado";
+}
 
 
 
